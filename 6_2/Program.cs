a6f60a0e3d7cccfcdcce67d6c1866e8f3fdc479a
Avenir Baryshev <avenirpik@gmail.com>
2025-03-23 20:36:47 +0700

    2 rdy

---
+++ 6_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Text;
+
+namespace _6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение без знаков препинания, разделитель - пробел");
+            string vvod = Console.ReadLine();
+            string reverse = string.Empty;
+
+            string normal = vvod.Replace(" ", "");
+            normal = normal.ToLower();
+
+            for (int i = normal.Length - 1; i >= 0; i--)
+            {
+                reverse += normal[i];
+            }
+
+            Console.WriteLine("\n{0} - исходное предложение", normal);
+            Console.WriteLine("\n{0} - реверсивное предложение", reverse);
+
+            if (reverse == normal)
+            {
+                Console.WriteLine("\nУРА! Это полиндром ! =)");
+            }
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
