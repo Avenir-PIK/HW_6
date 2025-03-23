5bb0493532716b0a75c233b4e5d81961a6c21504
Avenir Baryshev <avenirpik@gmail.com>
2025-03-23 20:12:17 +0700

    1 rdy

--- 6_1/Program.cs
+++ 6_1/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace _6_1
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace _6_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //gogogo 
+            Console.WriteLine("Введите предложение без знаков препинания, разделитель - пробел");
+            string input = Console.ReadLine();
+            string[] words = input.Split(' ');
+            int max = 0;
+            string bigword = string.Empty;
+
+            foreach (string word in words)
+            {
+                if (word.Length > max)
+                {
+                    max = word.Length;
+                    bigword = word;
+                }
+            }
+                Console.WriteLine("\nСамое длинное слово: {0}\nСостоит из {1} букв", bigword, max);
+                Console.WriteLine("\nPress any key to continue");
+
+                Console.ReadKey();
+            }
         }
     }
-}
